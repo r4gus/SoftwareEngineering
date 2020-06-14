97ef72b2f2dc2b3f834e15ba5b8eb77800b41a3d
r4gus <foobarc11@protonmail.ch>
2020-06-14 21:40:08 +0200

    first commit

---
+++ studiengang.cpp
@@ -0,0 +1,6 @@
+#include "studiengang.h"
+
+Studiengang::Studiengang()
+{
+
+}
---
+++ studiengang.h
@@ -0,0 +1,11 @@
+#ifndef STUDIENGANG_H
+#define STUDIENGANG_H
+
+
+class Studiengang
+{
+public:
+    Studiengang();
+};
+
+#endif // STUDIENGANG_H
